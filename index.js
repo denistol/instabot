3510504b72cc48ce2f5b8db6129653b4bec02435
denistol <denistol@pm.me>
2020-08-23 21:47:36 +0300

    Upd

--- index.js
+++ index.js
@@ -1,55 +1,59 @@
 const path = require('path')
 const fs = require('fs')
 const puppeteer = require('puppeteer');
-const envPath = path.join(__dirname, '.env')
-require('dotenv').config({ path: envPath })
 
-const TAGS = ['follow', 'food', 'fun', 'follow4follow', 'followme']
+const configBuffer = fs.readFileSync(path.join(__dirname, 'config.json'))
+const config = JSON.parse(configBuffer.toString())
 const LOGFILE = `likes.log`
-const TIMEOUT = 60000
+const TIMEOUT = Number(config.delayPerAction * 1000)
 
 class Bot {
   constructor(email, password) {
     this.likes = 0
     this.BASE_URL = 'https://instagram.com'
     this.email = email
-    this.tags = TAGS
+    this.tags = config.tags
     this.password = password
     this.run();
   }
   getRandomTag() {
     return this.tags[Math.floor(Math.random() * this.tags.length)];
   }
-  async run () {
+  async run() {
     await this.init();
     await this.login();
-    for(let i of Array(150000)) {
+    for (let i of Array(config.actions)) {
       await this.exploreTag();
       await this.page.waitFor(TIMEOUT)
     }
   }
   async init() {
-    this.browser = await puppeteer.launch({ headless: true })
+    this.browser = await puppeteer.launch({ headless: false })
     this.page = await this.browser.newPage()
   }
   async login() {
     await this.page.goto(this.BASE_URL)
     await this.page.waitFor(`input[name="username"]`)
-    await this.page.type(`input[name="username"]`, this.email, {delay: 50})
-    await this.page.type(`input[name="password"]`, this.password, {delay: 50})
-    await this.page.click(`button[type="submit"]`, {delay: 50})
+    await this.page.type(`input[name="username"]`, this.email, { delay: 50 })
+    await this.page.type(`input[name="password"]`, this.password, { delay: 50 })
+    await this.page.click(`button[type="submit"]`, { delay: 50 })
     await this.page.waitFor(`[aria-label="Home"]`)
   }
   async likePost(linkText) {
     const noLike = await this.page.$$(`article section:nth-child(1) button svg[aria-label="Like"]`)
-    if( noLike ) {
-      await this.page.click(`article section:nth-child(1) button`)
-      this.likes ++
-      if(linkText) {
-        const p = path.join(__dirname, LOGFILE)
-        fs.appendFileSync(p, `${linkText.trim()}\n`)
+    if (noLike) {
+      try {
+        await this.page.click(`article section:nth-child(1) button`)
+        this.likes++
+        if (linkText) {
+          const p = path.join(__dirname, LOGFILE)
+          fs.appendFileSync(p, `${linkText.trim()}\n`)
+        }
+        console.log('Likes:', this.likes)
       }
-      console.log('Likes:', this.likes)
+      catch {
+        console.log('[*] Like error')
+      }
     }
     await this.page.waitFor(1000)
   }
@@ -59,8 +63,8 @@ class Bot {
     await this.page.waitFor(mostRecent)
     const images = await this.page.$$(mostRecent)
     // like first 3 images
-    if(images?.length >= 3) {
-      for(let img of [...images.slice(0,3)]){
+    if (images?.length >= 3) {
+      for (let img of [...images.slice(0, 3)]) {
         const link = await img.getProperty('href')
         const linkText = await link.jsonValue()
         img.click()
@@ -74,4 +78,5 @@ class Bot {
     }
   }
 }
-const b = new Bot(process.env.EMAIL, process.env.PASSWORD);
+
+const b = new Bot(config.email, config.password);
